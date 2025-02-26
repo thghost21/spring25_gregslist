b2ada56eee8f00c8eaf3b7f16236c6610c849e24
Jeremy Fowler <jembyfowler@gmail.com>
2025-02-26 09:21:57 -0700

    example stuff is gone

--- app/App.js
+++ app/App.js
@@ -1,8 +1,7 @@
-import { ExampleController } from './controllers/ExampleController.js';
 
 class App {
 
-  ExampleController = new ExampleController() // ☑️ you can remove this - example only
+
 
 }
 
--- app/AppState.js
+++ app/AppState.js
@@ -3,8 +3,6 @@ import { createObservableProxy } from './utils/ObservableProxy.js'
 
 class ObservableAppState extends EventEmitter {
 
-  /**@type {import('./models/Example.js').Example[]} */
-  examples = []
 }
 
 export const AppState = createObservableProxy(new ObservableAppState())
